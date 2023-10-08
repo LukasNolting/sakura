d86753f239ae4749dd7b3d0f7733767162062ab1
LukasNolting <104640489+LukasNolting@users.noreply.github.com>
2023-10-08 14:04:31 +0200

    flags_languages

---
+++ script/script.js
@@ -0,0 +1,22 @@
+function german() {
+  document.getElementById("menu").innerHTML = "MENÜ";
+  document.getElementById("location").innerHTML = "STANDORT";
+  document.getElementById("about").innerHTML = "ÜBER UNS";
+  document.getElementById("ramen").innerHTML = "DAS BESTE RAMEN IN DER STADT";
+  document.getElementById("howto").innerHTML = "WIE BESTELLE ICH?";
+  document.getElementById("noodle").innerHTML = "<br>Wähle<br>Deine<br>Nudeln";
+  document.getElementById("broth").innerHTML = "<br>Wähle<br>Dein<br>Brot";
+  document.getElementById("toppings").innerHTML =
+    "<br>Wähle<br>Deine<br>Beilagen";
+  document.getElementById("our-menu").innerHTML = "UNSER MENÜ";
+  document.getElementById("chicken-tonkotsu").innerHTML = "HÄHNCHEN";
+  document.getElementById("beef-tonkotsu").innerHTML = "FLEISCH";
+  document.getElementById("shrimp-tonkotsu").innerHTML = "GARNELEN";
+  document.getElementById("chicken-spicy").innerHTML = "HÄHNCHEN";
+  document.getElementById("beef-spicy").innerHTML = "FLEISCH";
+  document.getElementById("shrimp-spicy").innerHTML = "GARNELEN";
+  document.getElementById("chicken-shio").innerHTML = "HÄHNCHEN";
+  document.getElementById("beef-shio").innerHTML = "FLEISCH";
+  document.getElementById("shrimp-shio").innerHTML = "GARNELEN";
+  document.getElementById("findus").innerHTML = "BESUCHE UNS";
+}
